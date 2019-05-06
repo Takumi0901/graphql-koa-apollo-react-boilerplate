e2fad5d64877c1437c89fef6c9272d9165ec63b1
takumi kimura <juventus0901@gmail.com>
2019-05-06 21:33:53 +0900

    Fix gen/actions

--- client/src/gen/actions.tsx
+++ client/src/gen/actions.tsx
@@ -1,6 +1,18 @@
 import gql from 'graphql-tag'
 import * as ReactApolloHooks from 'react-apollo-hooks'
 
+export const UsersDocument = gql`
+  query users {
+    users {
+      id
+      name
+    }
+  }
+`
+
+export function useUsersQuery(baseOptions?: ReactApolloHooks.QueryHookOptions<UsersQueryVariables>) {
+  return ReactApolloHooks.useQuery<UsersQuery, UsersQueryVariables>(UsersDocument, baseOptions)
+}
 export type Maybe<T> = T | null
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
@@ -23,17 +35,4 @@ export type UsersQueryVariables = {}
 
 export type UsersQuery = { __typename?: 'Query' } & {
   users: Maybe<Array<Maybe<{ __typename?: 'User' } & Pick<User, 'id' | 'name'>>>>
-}
-
-export const UsersDocument = gql`
-  query users {
-    users {
-      id
-      name
-    }
-  }
-`
-
-export function useUsersQuery(baseOptions?: ReactApolloHooks.QueryHookOptions<UsersQueryVariables>) {
-  return ReactApolloHooks.useQuery<UsersQuery, UsersQueryVariables>(UsersDocument, baseOptions)
 }
