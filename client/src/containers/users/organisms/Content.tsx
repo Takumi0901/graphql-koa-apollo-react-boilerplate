67d953135eb9e6e6837fd437e09f83defc3e97a3
takumi kimura <juventus0901@gmail.com>
2019-05-13 11:18:18 +0900

    Fix userList

--- client/src/containers/users/organisms/Content.tsx
+++ client/src/containers/users/organisms/Content.tsx
@@ -5,21 +5,20 @@ import Loading from 'src/components/atoms/Loading'
 const Content: React.FunctionComponent<{}> = () => {
   const { data, loading } = useUsersQuery()
   if (loading) return <Loading fetching={loading} />
-  if (data && Object.keys(data).length < 1) return null
-  return (
-    <React.Fragment>
-      {data &&
-        data.users &&
-        data.users.map((e, key) => {
-          if (!e) return null
+  if (data && data.users) {
+    return (
+      <React.Fragment>
+        {data.users.map((e, key) => {
           return (
             <div key={key}>
               <p>{e.name}</p>
             </div>
           )
         })}
-    </React.Fragment>
-  )
+      </React.Fragment>
+    )
+  }
+  return null
 }
 
 export default Content
